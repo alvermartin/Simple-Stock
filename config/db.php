5fec969b15875d19bce218c1adde203f5b7831e9
jabelloc1 <73957986+jabelloc1@users.noreply.github.com>
2020-11-17 16:41:15 -0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-/*Datos de conexion a la base de datos*/
+/*Datos de conexion a la base de datos
 define('DB_HOST', '4c003ee5a1eb');//DB_HOST:  generalmente suele ser "127.0.0.1"
 define('DB_USER', 'root');//Usuario de tu base de datos
 define('DB_PASS', 'root');//Contraseña del usuario de la base de datos
