a4360af54bca46400d1da72131efe93aa2863268
Alver Martin Rubiano Ruiz <alver.martin.rubiano.ruiz@gmail.com>
2020-11-17 21:19:39 -0500

    git

--- entrega 1/simplestock/www/config/db.php
+++ entrega 1/simplestock/www/config/db.php
@@ -1,6 +1,6 @@
 <?php
 /*Datos de conexion a la base de datos*/
-define('DB_HOST', '4c003ee5a1eb');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_HOST', 'f571a15d841f');//DB_HOST:  generalmente suele ser "127.0.0.1"
 define('DB_USER', 'root');//Usuario de tu base de datos
 define('DB_PASS', 'root');//Contraseña del usuario de la base de datos
 define('DB_NAME', 'CS');//Nombre de la base de datos
